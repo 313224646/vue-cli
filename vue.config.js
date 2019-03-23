ea4683dcce0cbff10ba9e147182f7dfb40a73651
w s h <wushanghai@zfgc.cn>
2019-03-23 16:49:02 +0800

    添加全局css配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   lintOnSave: false,
   css: {
@@ -7,5 +9,19 @@ module.exports = {
         }
       }
     }
+  },
+  chainWebpack: config => { // 全局引入样式表
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
   }
+}
+
+function addStyleResource(rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/css/global.styl')
+      ],
+    })
 }
