7e1dfed59e20b069a2528156059b78864934d2af
w s h <wushanghai@zfgc.cn>
2019-03-25 09:12:04 +0800

    修改配置

--- postcss.config.js
+++ postcss.config.js
@@ -15,7 +15,7 @@ module.exports = {
     "postcss-px-to-viewport": {
       viewportWidth: 375,
       viewportHeight: 667,
-      unitPrecision: 2,
+      unitPrecision: 3,
       viewportUnit: 'vw',
       selectorBlackList: ['.ignore', '.hairlines'],
       minPixelValue: 1,
