c3fc659e79f3f30751d7c8e045d568441acaf7d7
Shanghai <18620800179@163.com>
2019-05-15 16:22:13 +0800

    移除页面过渡监听

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,19 @@
 <template>
   <div id="app">
-    <transition :name="pageTransitionName">
-      <!-- 默认全局缓存页面 -->
-      <!-- 组件名字以-nka结尾都的文件都不缓存 “not keep alive” -->
-      <keep-alive :exclude="/-nka$/">
-        <router-view/>
-      </keep-alive>
-    </transition>
+    <!-- 默认全局缓存页面 -->
+    <!-- 组件名字以-nka结尾都的文件都不缓存 “not keep alive” -->
+    <keep-alive :exclude="/-nka$/">
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      pageTransitionName: 'refresh'
-    }
-  },
-  watch: {
-    '$route' (to, from) {
-      // 监听路由变更设置过渡效果
-      if (typeof from.meta.level === 'undefined') { // 判定为刷新操作
-        this.pageTransitionName = 'refresh'
-      } else {
-        const toLevel = parseInt(to.meta.level)
-        const fromLevel = parseInt(from.meta.level)
-        if (toLevel < fromLevel) { // 右滑
-          this.pageTransitionName = 'slide-right'
-        } else { // 左滑
-          this.pageTransitionName = 'slide-left'
-        }
-      }
-    }
-  }
+  name: 'app'
 }
 </script>
 
 <style lang="stylus">
-@import '~@/assets/stylus/page-transition.styl'
 @import '~@/assets/stylus/reset.styl'
 </style>
 
