3412897e01c1626fe33bc848ed0d722025c4d505
Shanghai <18620800179@163.com>
2019-05-15 11:52:18 +0800

    解决vant-ui引入报错

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  presets: [
-    '@vue/app'
-  ],
-  plugins: [  // 按需引入vant-ui设置
-    ["import", {
-      "libraryName": "vant",
-      "libraryDirectory": "es",
-      "style": name => `${name}/style/less`
-    }, 'vant']
+  presets: ['@vue/app'],
+  plugins: [ // 按需引入vant-ui设置
+    [
+      "import",
+      {
+        "libraryName": "vant",
+        "libraryDirectory": "es",
+        "style": true
+      },
+      'vant'
+    ]
   ]
 }
