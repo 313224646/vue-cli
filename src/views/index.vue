e0b8bff3f2af952b52db334c7d104b6da1bc3897
Shanghai <18620800179@163.com>
2019-07-03 14:19:51 +0800

    update

--- src/views/index.vue
+++ src/views/index.vue
@@ -5,10 +5,23 @@
 </template>
 
 <script>
+import axios from '@/plugins/axios'
 export default {
   name: 'home',
   components: {
     'common-text': () => import('@/components/common/common-text')
+  },
+  data () {
+    return {
+      list: []
+    }
+  },
+  created () {
+    this.axios.get('https://zsmall.zfgc.cn/f/wl/index/articles', {
+      params: {}
+    }).then(res => {
+      this.list = res
+    })
   }
 }
 </script>
